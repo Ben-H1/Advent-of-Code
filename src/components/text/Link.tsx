132290c768ef78ed9a22f9918ca6b7e6ce7eb4e1
Ben-H1 <benedicthawthorn@gmail.com>
2024-10-23 00:37:38 +0100

    Add custom class support for links

--- src/components/text/Link.tsx
+++ src/components/text/Link.tsx
@@ -1,13 +1,15 @@
+import clsx from 'clsx';
 import { ReactNode } from 'react';
 
 type LinkProps = {
     link: string;
     children?: ReactNode;
+    className?: string;
 };
 
-const Link = ({ link, children }: LinkProps) => {
+const Link = ({ link, children, className }: LinkProps) => {
     return (
-        <a href={link} target='_blank' className='text-aoc-link hover:underline'>
+        <a href={link} target='_blank' className={clsx('text-aoc-link hover:underline', className)}>
             {children}
         </a>
     );
