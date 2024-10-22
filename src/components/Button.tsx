5a129d53c82485e161e26828c0bb7e44741a0376
Ben-H1 <benedicthawthorn@gmail.com>
2024-10-23 00:37:31 +0100

    Add optional temporary text for buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,27 @@
 import clsx from 'clsx';
-import { MouseEventHandler } from 'react';
+import { MouseEventHandler, useState } from 'react';
 
 type ButtonProps = {
     onClick?: MouseEventHandler;
     children?: string | number | readonly string[] | undefined;
+    tempValue?: string | number | readonly string[] | undefined;
+    tempTimeMs?: number;
     className?: string;
 };
 
-const Button = ({ onClick, children, className }: ButtonProps) => {
+const Button = ({ onClick, children, tempValue, tempTimeMs = 1000, className }: ButtonProps) => {
+    const [useTemp, setTemp] = useState(false);
+
     return (
         <input
             type={'button'}
             className={clsx('px-4 py-2 rounded-lg outline-none cursor-pointer select-none bg-aoc-light hover:bg-aoc-medium active:bg-aoc-code', className)}
-            onClick={onClick}
-            value={children}
+            onClick={(...params) => {
+                setTemp(true);
+                setTimeout(() => setTemp(false), tempTimeMs);
+                onClick && onClick(...params);
+            }}
+            value={useTemp ? (tempValue ?? children) : children}
         />
     );
 };
