a5dbf03d3129b58d41cc33e010e807b76e723439
Ben-H1 <benedicthawthorn@gmail.com>
2024-10-24 18:43:52 +0100

    Fix bottom padding

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ const App = () => {
                 </div>
                 <div className='p-8 relative h-full w-full z-10'>
                     <AppRoutes />
+                    <div className='pb-8'></div>
                 </div>
             </div>
         </div>
