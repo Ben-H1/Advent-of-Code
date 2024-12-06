2b87fccacb5f76a81aa2203059ca695e43b5166a
Ben Hawthorn <benedicthawthorn@gmail.com>
2024-12-06 13:22:54 +0000

    Update array utility functions

--- src/util/array.ts
+++ src/util/array.ts
@@ -1,7 +1,10 @@
-export const indexOf2d = (array: any[][], value: any) => {
+import get from 'lodash.get';
+
+export const indexOf2d = (array: any[][], value: any, path?: string) => {
     for (let y = 0; y < array.length; y++) {
         for (let x = 0; x < array[0].length; x++) {
-            if (array[y][x] === value) {
+            const arrayValue = path ? get(array[y][x], path) : array[y][x];
+            if (arrayValue === value) {
                 return { x, y };
             }
         }
@@ -20,4 +23,23 @@ export const indexOfMulti = (array: any[], value: any) => {
     return indices;
 };
 
-export const stringify2d = (array: any[][], delimiter: string = '') => array.map(a => a.join(delimiter)).join('\n');
+type Stringify2dOptions = {
+    delimiter?: string;
+    mapper?: (cell?: any, x?: number, y?: number) => string | number;
+    path?: string;
+};
+
+export const stringify2d = (array: any[][], options?: Stringify2dOptions) => {
+    const delimiter = options?.delimiter;
+    const mapper = options?.mapper;
+    const path = options?.path;
+
+    return array
+        .map((row, y) => row
+            .map((cell, x) => mapper ? mapper(cell, x, y) : cell)
+            .map(cell => path ? get(cell, path) : cell)
+            .join(delimiter ?? '')
+        ).join('\n');
+};
+
+export const stringify2dOld = (array: any[][], delimiter: string = '') => array.map(a => a.join(delimiter)).join('\n');
