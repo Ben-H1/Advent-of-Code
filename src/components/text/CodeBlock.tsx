095a3a959a079d00a429dc196f46aa417e65ec6c
Ben Hawthorn <benedicthawthorn@gmail.com>
2026-01-05 13:20:46 +0000

    Add copy button to code block

--- src/components/text/CodeBlock.tsx
+++ src/components/text/CodeBlock.tsx
@@ -1,13 +1,30 @@
-import { ReactNode } from 'react';
+import IconButton from '@components/IconButton';
+import Tooltip from '@components/Tooltip';
+import { faCopy } from '@fortawesome/free-solid-svg-icons';
+import { ReactNode, useRef } from 'react';
 
 type CodeBlockProps = {
     children?: ReactNode;
 };
 
 const CodeBlock = ({ children }: CodeBlockProps) => {
+    const ref = useRef<HTMLDivElement | null>(null);
+
     return (
-        <div className='px-4 py-2 font-mono whitespace-pre border rounded-md w-fit bg-aoc-code text-aoc-text border-aoc-text'>
-            {children}
+        <div className='flex space-x-4 w-fit group'>
+            <div ref={ref} className='px-4 py-2 font-mono whitespace-pre border rounded-md w-fit bg-aoc-code text-aoc-text border-aoc-text'>
+                {children}
+            </div>
+            <Tooltip
+                tooltipContent='Copy'
+                tooltipTempContent='Copied!'
+            >
+                <IconButton
+                    className='invisible group-hover:visible'
+                    icon={faCopy}
+                    onClick={() => navigator.clipboard.writeText(ref.current?.innerText ?? '')}
+                />
+            </Tooltip>
         </div>
     );
 };
